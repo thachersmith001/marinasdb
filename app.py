974b4154376458b5ee81030504ea17238b42f945
thachersmith001 <109709694+thachersmith001@users.noreply.github.com>
2023-06-27 14:55:43 +0000

    who knows

--- app.py
+++ app.py
@@ -49,51 +49,39 @@ download_from_aws('marinasdatabase', 'urls.csv', 'urls.csv')
 
 # Read URLs from CSV
 with open('urls.csv', 'r') as f:
-  reader = csv.reader(f)
-  urls = list(reader)
+    reader = csv.reader(f)
+    urls = list(reader)
 
 # Open the output CSV file
 with open('marina_data.csv', 'w', newline='') as file:
-  writer = csv.writer(file)
-  # Write the headers
-  writer.writerow([
-    "Marina Name", "Phone Number", "Zip Code", "Total Slips",
-    "Transient Slips", "Daily Rate", "Weekly Rate", "Monthly Rate",
-    "Annual Rate"
-  ])
-
-  for url in urls:
-    url = url[0].lstrip('\ufeff')
-    response = requests.get(url)
-    soup = BeautifulSoup(response.text, 'html.parser')
-
-    # Extracting data
-    marina_name = soup.title.text.split(
-      '|')[0].strip() if soup.title else 'N/A'
-    phone_number = soup.find(
-      text='Reservations').find_next().text if soup.find(
-        text='Reservations') else 'N/A'
-    zip_code = soup.find(text='Zip:').find_next().text if soup.find(
-      text='Zip:') else 'N/A'
-    total_slips = soup.find(text='Total Slips:').find_next().text if soup.find(
-      text='Total Slips:') else 'N/A'
-    transient_slips = soup.find(
-      text='Transient Slips:').find_next().text if soup.find(
-        text='Transient Slips:') else 'N/A'
-    daily_rate = soup.find(text='Daily:').find_next().text if soup.find(
-      text='Daily:') else 'N/A'
-    weekly_rate = soup.find(text='Weekly:').find_next().text if soup.find(
-      text='Weekly:') else 'N/A'
-    monthly_rate = soup.find(text='Monthly:').find_next().text if soup.find(
-      text='Monthly:') else 'N/A'
-    annual_rate = soup.find(text='Annual:').find_next().text if soup.find(
-      text='Annual:') else 'N/A'
-
-    # Write the data to the CSV
+    writer = csv.writer(file)
+    # Write the headers
     writer.writerow([
-      marina_name, phone_number, zip_code, total_slips, transient_slips,
-      daily_rate, weekly_rate, monthly_rate, annual_rate
+        "Marina Name", "Phone Number", "Zip Code", "Total Slips", "Transient Slips",
+        "Daily Rate", "Weekly Rate", "Monthly Rate", "Annual Rate"
     ])
+
+    for url in urls:
+        url = url[0].lstrip('\ufeff')
+        response = requests.get(url)
+        soup = BeautifulSoup(response.text, 'html.parser')
+
+        # Extracting data
+        marina_name = soup.title.text.split('|')[0].strip() if soup.title else 'N/A'
+        phone_number = soup.find('a', {'class': 'phone'}).text if soup.find('a', {'class': 'phone'}) else 'N/A'
+        zip_code = soup.find('span', text='Zip:').find_next_sibling('span').text if soup.find('span', text='Zip:') else 'N/A'
+        total_slips = soup.find('span', text='Total Slips:').find_next_sibling('span').text if soup.find('span', text='Total Slips:') else 'N/A'
+        transient_slips = soup.find('span', text='Transient Slips:').find_next_sibling('span').text if soup.find('span', text='Transient Slips:') else 'N/A'
+        daily_rate = soup.find('span', text='Daily:').find_next_sibling('span').text if soup.find('span', text='Daily:') else 'N/A'
+        weekly_rate = 'N/A'  # Weekly rate is not provided on the page
+        monthly_rate = soup.find('span', text='Monthly:').find_next_sibling('span').text if soup.find('span', text='Monthly:') else 'N/A'
+        annual_rate = soup.find('span', text='Annual:').find_next_sibling('span').text if soup.find('span', text='Annual:') else 'N/A'
+
+        # Write the data to the CSV
+        writer.writerow([
+            marina_name, phone_number, zip_code, total_slips, transient_slips,
+            daily_rate, weekly_rate, monthly_rate, annual_rate
+        ])
 
 # Upload the CSV to AWS S3
 upload_to_aws('marina_data.csv', 'marinasdatabase', 'marina_data.csv')
