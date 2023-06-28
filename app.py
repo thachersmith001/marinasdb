b086749770c237b490f9b7fe85aed46232f79e82
thachersmith001 <109709694+thachersmith001@users.noreply.github.com>
2023-06-28 19:50:23 +0000

    na

--- app.py
+++ app.py
@@ -9,6 +9,7 @@ from botocore.exceptions import NoCredentialsError
 # Load the spaCy model
 nlp = spacy.load('en_core_web_sm')
 
+
 # AWS S3 upload function
 def upload_to_aws(local_file, bucket, s3_file):
   s3 = boto3.client(
@@ -52,31 +53,31 @@ download_from_aws('marinasdatabase', 'urls.csv', 'urls.csv')
 
 # Read URLs from CSV
 with open('urls.csv', 'r') as f:
-    reader = csv.reader(f)
-    urls = list(reader)
+  reader = csv.reader(f)
+  urls = list(reader)
 
 # Open the output CSV file
 with open('marina_data.csv', 'w', newline='') as file:
-    writer = csv.writer(file)
-    # Write the headers
-    writer.writerow([
-        "Marina Name", "Zip Code", "Daily Rate", "Weekly Rate", "Monthly Rate",
-        "Total Slips", "Transient Slips", "Fuel", "Repairs"
-    ])
+  writer = csv.writer(file)
+  # Write the headers
+  writer.writerow([
+    "Marina Name", "Zip Code", "Daily Rate", "Weekly Rate", "Monthly Rate",
+    "Total Slips", "Transient Slips", "Fuel", "Repairs"
+  ])
 
-    for url in urls:
-        url = url[0].lstrip('\ufeff')
-        # Make a request to the website
-        r = requests.get(url)
-        # Use the 'html.parser' to parse the page
-        soup = BeautifulSoup(r.content, 'html.parser')
-        # Extract the text from the page
-        text = soup.get_text()
-        # Use the NER model to extract entities
-        doc = nlp(text)
-        entities = [ent.text for ent in doc.ents]
-        # Write the entities to the CSV
-        writer.writerow(entities)
+  for url in urls:
+    url = url[0].lstrip('\ufeff')
+    # Make a request to the website
+    r = requests.get(url)
+    # Use the 'html.parser' to parse the page
+    soup = BeautifulSoup(r.content, 'html.parser')
+    # Extract the text from the page
+    text = soup.get_text()
+    # Use the NER model to extract entities
+    doc = nlp(text)
+    entities = [ent.text for ent in doc.ents]
+    # Write the entities to the CSV
+    writer.writerow(entities)
 
 # Upload the CSV to AWS S3
 upload_to_aws('marina_data.csv', 'marinasdatabase', 'marina_data.csv')
