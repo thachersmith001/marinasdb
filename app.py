11ff7f7fabfd636648a942afef663f0330853fa5
thachersmith001 <109709694+thachersmith001@users.noreply.github.com>
2023-06-28 21:01:01 +0000

    new guy w/openai

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
+import os
+import csv
+import boto3
+import re
+import requests
+import openai
 from botocore.exceptions import NoCredentialsError
-from autoscraper import AutoScraper
-
 
 # AWS S3 upload function
 def upload_to_aws(local_file, bucket, s3_file):
@@ -8,21 +12,6 @@ def upload_to_aws(local_file, bucket, s3_file):
         's3',
         aws_access_key_id=os.environ.get('AWS_ACCESS_KEY_ID'),
         aws_secret_access_key=os.environ.get('AWS_SECRET_ACCESS_KEY'))
-  s3 = boto3.client(
-    's3',
-    aws_access_key_id=os.environ.get('AWS_ACCESS_KEY_ID'),
-    aws_secret_access_key=os.environ.get('AWS_SECRET_ACCESS_KEY'))
-
-  try:
-    s3.upload_file(local_file, bucket, s3_file)
-    print("Upload Successful")
-    return True
-  except FileNotFoundError:
-    print("The file was not found")
-    return False
-  except NoCredentialsError:
-    print("Credentials not available")
-    return False
 
     try:
         s3.upload_file(local_file, bucket, s3_file)
@@ -41,21 +30,6 @@ def download_from_aws(bucket, s3_file, local_file):
         's3',
         aws_access_key_id=os.environ.get('AWS_ACCESS_KEY_ID'),
         aws_secret_access_key=os.environ.get('AWS_SECRET_ACCESS_KEY'))
-  s3 = boto3.client(
-    's3',
-    aws_access_key_id=os.environ.get('AWS_ACCESS_KEY_ID'),
-    aws_secret_access_key=os.environ.get('AWS_SECRET_ACCESS_KEY'))
-
-  try:
-    s3.download_file(bucket, s3_file, local_file)
-    print("Download Successful")
-    return True
-  except FileNotFoundError:
-    print("The file was not found")
-    return False
-  except NoCredentialsError:
-    print("Credentials not available")
-    return False
 
     try:
         s3.download_file(bucket, s3_file, local_file)
@@ -68,6 +42,23 @@ def download_from_aws(bucket, s3_file, local_file):
         print("Credentials not available")
         return False
 
+# Function to extract data from the page using OpenAI API
+def extract_data(page_content):
+    # Initialize the OpenAI API with your API key
+    openai.api_key = os.environ.get('OPENAI_API_KEY')
+
+    # Use the OpenAI API to extract the necessary data from the page content
+    response = openai.Completion.create(
+      engine="text-davinci-003",
+      prompt=page_content,
+      temperature=0.5,
+      max_tokens=200
+    )
+
+    # Parse the response to extract the necessary data
+    data = response.choices[0].text.strip()
+    return data
+
 # Download the CSV from AWS S3
 download_from_aws('marinasdatabase', 'urls.csv', 'urls.csv')
 
@@ -75,29 +66,6 @@ download_from_aws('marinasdatabase', 'urls.csv', 'urls.csv')
 with open('urls.csv', 'r') as f:
     reader = csv.reader(f)
     urls = list(reader)
-  reader = csv.reader(f)
-  urls = list(reader)
-
-# Define a list of sample data to train the scraper
-sample_data = [
-    'Bohicket Marina & Market', '29455', '$3.50 per ft.', '$35.00 per ft.', '$19.50 per ft.', '200', '25', 'Yes', 'Yes',
-    'Shelter Cove Marina', '29928', '$3.00 per ft.', '$18.00 per ft.', '$18.00 per ft.', '178', '30', 'Yes', 'Yes',
-    'Harbour Town Yacht Basin', '29928', '$3.25/ft Day', '$3.00/ft Weekly', '', '100', '', 'Yes', 'No'
-  'Bohicket Marina & Market', '29455', '$3.50 per ft.', '$35.00 per ft.',
-  '$19.50 per ft.', '200', '25', 'Yes', 'Yes', 'Shelter Cove Marina', '29928',
-  '$3.00 per ft.', '$18.00 per ft.', '$18.00 per ft.', '178', '30', 'Yes',
-  'Yes', 'Harbour Town Yacht Basin', '29928', '$3.25/ft Day',
-  '$3.00/ft Weekly', '', '100', '', 'Yes', 'No'
-]
-
-# Create a new AutoScraper
-scraper = AutoScraper()
-
-# Train the scraper on the sample data
-scraper.build('https://www.waterwayguide.com/marina/bohicket-marina-and-yacht-club', sample_data)
-scraper.build(
-  'https://www.waterwayguide.com/marina/bohicket-marina-and-yacht-club',
-  sample_data)
 
 # Open the output CSV file
 with open('marina_data.csv', 'w', newline='') as file:
@@ -106,25 +74,19 @@ with open('marina_data.csv', 'w', newline='') as file:
     writer.writerow([
         "Marina Name", "Zip Code", "Daily Rate", "Weekly Rate", "Monthly Rate", "Total Slips", "Transient Slips", "Fuel", "Repairs"
     ])
-  writer = csv.writer(file)
-  # Write the headers
-  writer.writerow([
-    "Marina Name", "Zip Code", "Daily Rate", "Weekly Rate", "Monthly Rate",
-    "Total Slips", "Transient Slips", "Fuel", "Repairs"
-  ])
 
     for url in urls:
         url = url[0].lstrip('\ufeff')
-        # Use the scraper to extract data from the URL
-        results = scraper.get_result_similar(url, grouped=False)
+        # Use requests to get the content of the page
+        response = requests.get(url)
+        content = response.text
+        # Use the OpenAI API to extract data from the URL
+        results = extract_data(content)
         # Write the data to the CSV
         writer.writerow(results)
-  for url in urls:
-    url = url[0].lstrip('\ufeff')
-    # Use the scraper to extract data from the URL
-    results = scraper.get_result_similar(url, grouped=False)
-    # Write the data to the CSV
-    writer.writerow(results)
+
+# Close the CSV file
+file.close()
 
 # Upload the CSV to AWS S3
 upload_to_aws('marina_data.csv', 'marinasdatabase', 'marina_data.csv')
