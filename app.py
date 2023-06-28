97ccc7e1a1fbd7fc1024261682265f7b3ad69056
thachersmith001 <109709694+thachersmith001@users.noreply.github.com>
2023-06-28 21:22:42 +0000

    prompt introduction

--- app.py
+++ app.py
@@ -53,10 +53,30 @@ def extract_data(page_content):
     h.ignore_links = True
     plaintext = h.handle(page_content)
 
+    # Reduce the prompt length
+    prompt = "Extract the following data from the page:\n"
+    prompt += "- Marina Name\n"
+    prompt += "- Zip Code\n"
+    prompt += "- Daily Rate\n"
+    prompt += "- Weekly Rate\n"
+    prompt += "- Monthly Rate\n"
+    prompt += "- Annual Rate\n"
+    prompt += "- Total Slips\n"
+    prompt += "- Transient Slips\n"
+    prompt += "- Fuel\n"
+    prompt += "- Repairs\n"
+    prompt += "- Phone Number\n"
+    prompt += "- Latitude\n"
+    prompt += "- Longitude\n"
+    prompt += "- Max Vessel Length\n"
+
+    # Combine the prompt and plaintext content
+    input_text = prompt + plaintext
+
     # Use the OpenAI API to extract the necessary data from the page content
     response = openai.Completion.create(
         engine="text-davinci-003",
-        prompt=plaintext,
+        prompt=input_text,
         temperature=0.5,
         max_tokens=200
     )
@@ -73,30 +93,28 @@ with open('urls.csv', 'r') as f:
     reader = csv.reader(f)
     urls = list(reader)
 
-# Initialize the AWS S3 client
-s3 = boto3.client(
-    's3',
-    aws_access_key_id=os.environ.get('AWS_ACCESS_KEY_ID'),
-    aws_secret_access_key=os.environ.get('AWS_SECRET_ACCESS_KEY')
-)
+# Open the output CSV file
+with open('marina_data.csv', 'w', newline='') as file:
+    writer = csv.writer(file)
+    # Write the headers
+    writer.writerow([
+        "Marina Name", "Zip Code", "Daily Rate", "Weekly Rate", "Monthly Rate", "Annual Rate",
+        "Total Slips", "Transient Slips", "Fuel", "Repairs", "Phone Number", "Latitude", "Longitude",
+        "Max Vessel Length"
+    ])
 
-# Process each URL
-for url in urls:
-    url = url[0].lstrip('\ufeff')
+    for url in urls:
+        url = url[0].lstrip('\ufeff')
+        # Use requests to get the content of the page
+        response = requests.get(url)
+        content = response.text
+        # Use the OpenAI API to extract data from the URL
+        results = extract_data(content)
+        # Write the data to the CSV
+        writer.writerow(results)
 
-    # Use requests to get the content of the page
-    response = requests.get(url)
-    content = response.text
-
-    # Convert the HTML content to plaintext
-    h = html2text.HTML2Text()
-    h.ignore_links = True
-    plaintext = h.handle(content)
-
-    # Save the plaintext content to AWS S3 with the filename as the URL
-    filename = re.sub(r'\W+', '', url) + '.txt'
-    s3.put_object(Body=plaintext, Bucket='marinasdatabase', Key=filename)
-    print(f"Saved plaintext content to {filename}")
+# Close the CSV file
+file.close()
 
 # Upload the CSV to AWS S3
 upload_to_aws('marina_data.csv', 'marinasdatabase', 'marina_data.csv')
