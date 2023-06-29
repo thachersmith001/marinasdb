ebeca59d04a6a91cd8977c23fd3971a84a54c8cb
thachersmith001 <109709694+thachersmith001@users.noreply.github.com>
2023-06-29 06:41:31 +0000

    dd

--- hotels.py
+++ hotels.py
@@ -9,25 +9,27 @@ s3 = boto3.client('s3')
 
 
 def dms2dd(s):
-  # Remove any non-standard characters
-  s = ''.join(c for c in s if c.isdigit() or c in ['-', ' ', '°', "'"])
+    # Remove any non-standard characters
+    s = ''.join(c for c in s if c.isdigit() or c in ['-', ' ', '°', "'"])
 
-  parts = s.strip().split('°')
-  degrees = parts[0].strip()
+    parts = s.strip().split('°')
+    degrees = parts[0].strip()
 
-  # We should check if there's a space in the degrees part and split again if there is.
-  if ' ' in degrees:
-    parts = degrees.split(' ')
-    degrees = parts[0]
-    if len(
-        parts
-    ) > 1:  # If there are more parts after splitting on space, add it back to parts for further processing
-      parts[1] = parts[1] + '°'
-      parts = parts[1:] + parts[2:]
+    # We should check if there's a space in the degrees part and split again if there is.
+    if ' ' in degrees:
+        parts = degrees.split(' ')
+        degrees = parts[0]
+        if len(parts) > 1:  # If there are more parts after splitting on space, add it back to parts for further processing
+            parts[1] = parts[1] + '°'
+            parts = parts[1:] + parts[2:]
 
-  minutes = parts[1].replace("'", "")
-  return float(degrees) + float(minutes) / 60
-
+    # Only process minutes if we have them in the parts list.
+    if len(parts) > 1:
+        minutes = parts[1].replace("'", "")
+        return float(degrees) + float(minutes) / 60
+    else:
+        # If no minutes part exists, just return the degrees as float.
+        return float(degrees)
 
 def process_coordinates(s):
   coord = s.replace("N", "").replace("W", "-").strip().split(',')
